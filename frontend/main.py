a4d621ce3d170eae4a90d0921f21ee9ab3dd8363
Anton Karazeev <anton.karazeev@gmail.com>
2017-11-25 22:59:24 +0300

    Up index.html

--- frontend/main.py
+++ frontend/main.py
@@ -1,25 +1,53 @@
 from flask import Flask, render_template, request
-from engine import get_df, process_query
-from utils import get_data
+from utils import get_data, get_jsdata
+import datetime
 
 app = Flask(__name__)
-df = get_df()
 data = get_data()
+jsdata = get_jsdata()
 
 authors = sorted(list(set(data['Author / Tekijä'].values)))
+cases = list(jsdata.keys())
+
+
+@app.route('/login', methods=['get', 'post'])
+def login_form():
+    # return render_template('index_prod.html')
+    return render_template('login_form.html')
+
+    ########################
+    # if request.form:
+    #     query = request.form.get('query')
+    #     print(request.form.get('select'))
+    #     entries = process_query(query, df)
+    #     print(len(entries))
+    #     return render_template('index.html', authors=authors, cases=cases, query=query, entries=entries)
+    ##############################
+
+    # return render_template('index.html', authors=authors, cases=cases,
+    #                        transactions=transactions, total_for_transactions=total_for_transactions,
+    #                        info=info)
 
 
 @app.route('/', methods=['get', 'post'])
 def index():
-    if request.form:
-        query = request.form.get('query')
-        print(request.form.get('select'))
-        entries = process_query(query, df)
-        print(len(entries))
-        return render_template('new_index.html', authors=authors, query=query, entries=entries)
 
-    return render_template('new_index.html', authors=authors)
-
+    # project_id = '20120036'
+    # info = jsdata[project_id]['Info']
+    #
+    # transactions = jsdata[project_id]['Transactions']
+    # transactions = list(transactions.values())
+    #
+    # # Sort the transactions by date
+    # transactions = sorted(transactions, key=lambda x: datetime.datetime.strptime(x['Entry Date'], '%Y-%m-%d'))
+    #
+    # total_for_transactions = '{:.2f}'.format(sum([trans['Total'] for trans in transactions]))
+    #
+    # return render_template('index_prod.html', authors=authors, cases=cases,
+    #                        transactions=transactions, total_for_transactions=total_for_transactions,
+    #                        info=info)
+    return render_template('index_prod.html', authors=authors)
+    # return render_template('index_prod.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/utils.py
+++ frontend/utils.py
@@ -1,9 +1,19 @@
 import pandas as pd
-
+import json
 
 def get_data():
     """
-    Load .xlsx file as dataframe
+    Load .xlsx file as dataframe.
     :return:
     """
     return pd.read_excel('data.xlsx', sheetname='Sheet2')
+
+
+def get_jsdata():
+    """
+    Load .json file as dict.
+    :return:
+    """
+    with open('json_1', 'r') as f:
+        jsdata = json.load(f)
+    return jsdata
