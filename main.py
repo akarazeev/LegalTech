14d079da801ab560e55f2cf84ed391f07b5a5aa6
Anton Karazeev <anton.karazeev@gmail.com>
2017-11-26 11:31:35 +0300

    Up

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request
 from utils import get_data, get_jsdata
+from myemail import send_email, make_html
 import datetime
+import typing
 
 app = Flask(__name__)
+
+# Read the data
 data = get_data()
 jsdata = get_jsdata()
 
-authors = sorted(list(set(data['Author / Tekijä'].values)))
-cases = list(jsdata.keys())
+# Getting cases
+cases = ['-- Select the case --']
+for id_assignment in jsdata:
+    cases.append(' -'.join((id_assignment, jsdata[id_assignment]['Info']['Assigment name'])))
 
 username = 'Anton Karazeev'
+server_address = "http://10.100.23.13:5000"
+
+with open('user_psswd_recipient.txt', 'r') as f:
+    user = f.readline().strip()
+    password = f.readline().strip()
+    recipient = f.readline().strip()
 
 
 @app.route('/login', methods=['get', 'post'])
@@ -19,12 +31,15 @@ def login_form():
 
 @app.route('/', methods=['get', 'post'])
 def index():
-    project_id = '20120036'
+    project_id = None
 
     if request.form:
-        project_id = request.form.get('query')
+        project_id = request.form.get('select_cases')
+        project_id = project_id.split('-')[0].strip()
+    else:
+        project_id = '20120036'
 
-    if project_id in jsdata:
+    if project_id in jsdata.keys():
         info = jsdata[project_id]['Info']
 
         transactions = jsdata[project_id]['Transactions']
@@ -35,23 +50,47 @@ def index():
 
         total_for_transactions = '{:.2f}'.format(sum([trans['Total'] for trans in transactions]))
 
-        return render_template('index_prod.html', query=project_id, transactions=transactions,
+        return render_template('index_prod.html', cases=cases, query=project_id, transactions=transactions,
                                total_for_transactions=total_for_transactions,
                                info=info, username=username)
     else:
-        return render_template('index_prod.html', query=project_id, not_found=True, username=username)
+        return render_template('index_prod.html', cases=cases, query=project_id, not_found=True, username=username)
 
 
-# request_form.html
+def is_email(address: str) -> bool:
+    if "@" in address and len(address.split('@')) == 2 and '/' not in address:
+        return True
+    else:
+        return False
+
+
 @app.route('/form', methods=['get', 'post'])
 def send_request():
     if request.form:
         subject = request.form.get('subject')
         body = request.form.get('body')
+        address = request.form.get('address')
+
         print(subject, body)
+
+        # Sending an email
+        if is_email(address):
+            yes_url = server_address + '/accept'
+            modif_title = 'Re: {}'.format(subject)
+            modifications_url = 'mailto:{}?subject={}&body=Original text: {}'.format(user, modif_title, body)
+            html = make_html(subject, body, yes_url, modifications_url)
+            send_email(user, password, address, subject, html)
+        # Sending a message using chat-bot
+        else:
+            pass
         return render_template('request_form.html', sent=True, username=username)
     return render_template('request_form.html', username=username)
 
 
+@app.route('/accept', methods=['get', 'post'])
+def accept_request():
+    return render_template('accept.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ myemail.py
@@ -0,0 +1,65 @@
+# Requires file user_psswd_recipient.txt with: user, password, recipient on new lines
+
+import urllib
+import urllib.request
+import requests
+import sys
+import email
+import smtplib
+from bs4 import BeautifulSoup
+from time import sleep
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_email(user, pwd, recipient, subject, body):
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = [recipient]
+    SUBJECT = subject
+
+    msg = MIMEMultipart("alternative")
+    msg.set_charset("utf-8")
+
+    msg["Subject"] = SUBJECT
+    msg["From"] = FROM
+    msg["To"] = TO[0]
+
+    TEXT = MIMEText(body, "html", "utf-8")
+
+    msg.attach(TEXT)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_user, gmail_pwd)
+
+    server.sendmail(FROM, TO, msg.as_string())
+
+    server.close()
+    print("Success - {}".format(SUBJECT))
+
+
+def make_html(title, text, yes_url, modifications_url):
+    html = """\
+    <html>
+      <head></head>
+      <body>
+          <table>
+              <tr>
+                  <td align='center' colspan="2"><b style="font-size:20px">{}</b><br></td>
+              </tr>
+              <tr>
+                  <td align='center' colspan="2">{}</td>
+              </tr>
+              <tr>
+                  <td align='center' style="width:50px"><a href="{}">Yes</a></td>
+                  <td align='center' style="width:50px"><a href="{}">Modifications</a></td>
+              </tr>
+          </table>
+      </body>
+    </html>
+    """.format(title, text, yes_url, modifications_url)
+
+    return html
